316a4737f4c174c75312b8a49988b631a511d5ac
TesseractA <tesseractwxyz@gmail.com>
2019-07-13 19:53:42 -0400

    Update PGN_interpreter.py
    More complete with internal cross-table representation. The next step is to give sensible outputs that are required for a function to make formatted tables with head-to-head statistics, then another dimension of representation is to be added to allow differentiation between time controls.

--- PGN_interpreter.py
+++ PGN_interpreter.py
@@ -1,28 +1,38 @@
 # Writing functions
 
-# This will read the string from a PGN, look for which 
-# engines are white and black, and will take scores to assign to the scoreboard.
+# After read and translate is finished, take output
+# from translate and write it into a file that can be
+# stored as text
 def interpret(file_string): 
     i = 0 # index
-    numberOfEngineGames = 0
+    numberOfGamesTable = []
+    # colorCrossTable = [] which can be used for wins for scoring wins as black or white heavier if the feature is wanted
+    trueCrossTable = [] # it's a real crosstable!
     engineNameList = []
     engineName = ""
     engineIndexWhite = 0 # used for identifying the index of an engine in engineNameList
     engineIndexBlack = 0
+    score = 0
+    engineScores = []
     while i < len(file_string):
-        if file_string[i, i+7] == "White \"" or file_string[i, i+7] == "Black \"":
+        if file_string[i:i+7] == "White \"":
             # go 7 characters forward to skip 'White "'
             i += 7
             engineName = read_engineName(file_string, engineNameList, i)
             engineNameList = check_engineName(engineName, engineNameList)
-             = i,  = detect_score(file_string, i)
+            engineIndexWhite = detect_engineIndex(engineNameList, engineName)
+        elif file_string[i:i+7] == "Black \"":
+            i += 7
+            engineName = read_engineName(file_string, engineNameList, i)
+            engineNameList = check_engineName(engineName, engineNameList)
+            engineIndexBlack = detect_engineIndex(engineNameList, engineName)
+            score = detect_score(file_string, i)
+            # colorCrossTable[engineIndexWhite][engineIndexBlack] += score for intentionally for as white/black winrates
+            trueCrossTable[engineIndexWhite][engineIndexBlack] += score
+            trueCrossTable[engineIndexBlack][engineIndexWhite] += 1-score
+            score = 0
         ++i
     i = 0
-    while i < len(file_string):
-        if n == file_string[i:i+engineNameLength]:  
-            for n in engineNameList:
-                engineNameLength = len(n)
-    
     
         
 def read_engineName(file_string, engineName, index):
@@ -33,23 +43,24 @@ def read_engineName(file_string, engineName, index):
     return engineName
 def check_engineName(engineName, engineNameList):
     icheck = 0 # engine name index
-    engineNameList_length = len(engineNameList)
-    isRecordedAlready = False
-    while icheck <= engineNameList_length:
-        if icheck == engineNameList_length:
+    engineNameListLength = len(engineNameList)
+    while icheck <= engineNameListLength:
+        if icheck == engineNameListLength:
             engineNameList += engineName
             # if the engine's name wasn't detected in the list already
         elif engineName == engineNameList[icheck]:
-            isRecordedAlready = True
             break
-    return engineNameList, isRecordedAlready
+    return engineNameList
 def detect_engineIndex(engineNameList, engineName):
     index = 0
+    engineIndex = 0
     while True:
         if engineName == engineNameList:
-            
+            engineIndex = index
+            break
         else:
-            index++
+            ++index
+    return engineIndex
             
             
 def detect_score(file_string, index):
@@ -67,3 +78,23 @@ def detect_score(file_string, index):
         else:
             ++index
     return index, score
+
+
+def crosstableVisual(trueCrossTable, numberOfGamesCrosstable):
+    # this will create a visual representation
+    nice = 1
+def detectBulletRapidClassical(file_string):
+    i = 0
+    while i < len(file_string):
+        nice = 1
+
+
+def I_READ(a_filename):
+    filename = open(a_filename, "r")
+    longest_string_in_python = filename.read()
+    return longest_string_in_python
+
+def I_WRITE(a_filename):
+    filename = open("a_filename.txt", "w+")
+    longest_string_in_python = filename.write()
+    
