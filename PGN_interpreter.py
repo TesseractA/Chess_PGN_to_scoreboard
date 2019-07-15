116b493884a1d761bbc40208476241f6c7361b1a
TesseractA <tesseractwxyz@gmail.com>
2019-07-14 22:43:30 -0400

    Update PGN_interpreter.py
    Customizable Time controls, and handles different time control cases. It won't work right now, but it's on it's way.

--- PGN_interpreter.py
+++ PGN_interpreter.py
@@ -7,34 +7,43 @@ def interpret(file_string, engineNameList):  # the engineNameList does not have 
     i = 0 # index
     # engineNameList = engineNameList not sure if this is necessary
     # colorCrossTable = [] which can be used for wins for scoring wins as black or white heavier if the feature is wanted
-    trueCrossTable = [] # it's a real crosstable!
+    trueCrossTable = [] # it's a real crosstable! ok, it's just a placeholder.
+    bulletCrossTable = [] #verbatim
+    rapidCrossTable = [] #yep
+    classicalCrossTable = [] #having fun here
     engineName = ""
     engineIndexWhite = 0 # used for identifying the index of an engine in engineNameList
     engineIndexBlack = 0
     score = 0
     bulletRapidClassical = 0 # will be determined by a game name.
     while i < len(file_string):
-        if file_string[i:i+7] == "White \"":
+        if file_string[i:i+8] == "White \"":
             # go 7 characters forward to skip 'White "'
             i += 7
             engineName = read_engineName(file_string, engineNameList, i)
             engineNameList = add_engineName(engineName, engineNameList)
             engineIndexWhite = detect_engineIndex(engineNameList, engineName)
-        elif file_string[i:i+7] == "Black \"":
+        elif file_string[i:i+8] == "Black \"":
             i += 7
             engineName = read_engineName(file_string, engineNameList, i)
             engineNameList = add_engineName(engineName, engineNameList)
             engineIndexBlack = detect_engineIndex(engineNameList, engineName)
-            i, score = detect_score(file_string, i)
+            score, i = detect_score(file_string, i)
             # colorCrossTable[engineIndexWhite][engineIndexBlack] += score for intentionally for as white/black winrates
             trueCrossTable[engineIndexWhite][engineIndexBlack] += score
             trueCrossTable[engineIndexBlack][engineIndexWhite] += 1-score
             score = 0
         elif file_string[i:i+5] == "Event":
-            i, bulletRapidClassical = detectBulletRapidClassical(file_string, i)
+            bulletRapidClassical, i = detectTimeControl(file_string, i)
+            if bulletRapidClassical == 1:    
+                trueCrossTable = bulletCrossTable
+            if bulletRapidClassical == 2:
+                trueCrossTable = rapidCrossTable
+            if bulletRapidClassical == 3:
+                trueCrossTable = classicalCrossTable
         ++i
     i = 0
-    
+    return bulletRapidClassical, bulletCrossTable, rapidCrossTable, classicalCrossTable
         
 def read_engineName(file_string, engineName, index):
     engineNameLength = 0
@@ -90,7 +99,7 @@ def detect_score(file_string, index):
             break
         else:
             ++index
-    return index, score
+    return score, index
 
 
 def makeScoreboard(bulletCrossTable, rapidCrossTable, classicalCrossTable, engineNameList, engineIndex1, engineIndex2): # returns a string
@@ -140,32 +149,69 @@ def makeScoreboard(bulletCrossTable, rapidCrossTable, classicalCrossTable, engin
     # end of line 4
     + "Overall                               " + weightedScoreOne + "                " + weightedScoreTwo
     # end of line 5
+    print(crossTableVisual)
     return crossTableVisual
     
 def amountOfGames(realCrossTable, engineIndex1, engineIndex2): # will return amount of games played between 2 engines
     gameAmount = realCrossTable[engineIndex1][engineIndex2] + realCrossTable[engineIndex1][engineIndex2]
     return gameAmount
     
-def detectBulletRapidClassical(file_string, i):
+def detectTimeControl(file_string, i):
     # 1 = bullet 2 = rapid 3 = classical while 0 probably indicates an error.
     bulletRapidClassical = 0
     while i < len(file_string):
         if file_string[i] == "|":
-            if file_string[i-1:i+1]:
+            expectedMovesConstant = 40
+            # this will assume a game length of 40 moves -- the same as human games.
+            initialTime = 0 # in minutes
+            incrementTime = 0 # in seconds, will be multiplied by expectedMovesConstant
+            initialTimeDigits = 0 # used to find out whether a symbol should be interpreted or not
+            incrementTimeDigits = 0 # ^
+            expectedTime = 0
+            if (is_number(file_string[i-1]) and is_number(file_string[i+1])): 
+                # if both numbers are right next to the "|"
+                if is_number(file_string[i+2]): 
+                    incrementTimeDigits = 2
+                if is_number(file_string[i-2]): 
+                    initialTimeDigits = 2
+                initialTime = file_string[i+initialTimeDigits]
+                incrementTime = file_string[i+incrementTimeDigits]
+            elif (is_number(file_string[i-2]) and is_number(file_string[i+2])): 
+                # if both numbers are a spacebar away from what was expected
+                if is_number(file_string[i+3]): 
+                    incrementTimeDigits = 2
+                if is_number(file_string[i-3]): 
+                    initialTimeDigits = 2
+                initialTime = file_string[i+initialTimeDigits]
+                incrementTime = file_string[i+incrementTimeDigits]
+            else:
+                #if there was no time control detected
+                continue
+            expectedTime = initialTime * 60 + incrementTime * expectedMovesConstant
+            if expectedTime < 360:
                 bulletRapidClassical = 1
-            elif file_string[i-1:i+1]:
+            elif expectedTime < 540:
                 bulletRapidClassical = 2
-            elif file_string[i-1:i+1]:
+            else:
                 bulletRapidClassical = 3
-        if file_string[i:i+7] == "Bullet" or file_string[i:i+4] == "1|2":
+        elif file_string[i:i+7] == "Bullet":
             bulletRapidClassical = 1
             break
-        elif file_string[i:i+8] == "Rapid" or file_string[i:i+4] == "5|2":
+        elif file_string[i:i+8] == "Rapid":
             bulletRapidClassical = 2
             break
-        elif file_string[i:i+4] == "Classical" or file_string[i:i+5] == "15|3":
+        elif file_string[i:i+4] == "Classical":
             bulletRapidClassical = 3
             break
+        ++i
+    return i, bulletRapidClassical
+
+
+def is_number(string): # returns a boolean whether the string is a number or not
+    if string == '1' or '2' or '3' or '4' or '5' or '6' or '7' or '8' or '9':
+        return True
+    else:
+        return False
 
 
 def I_READ(a_filename):
